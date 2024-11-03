ddfb09c0696d682c85cf2f1b70011620aefa2c4f
Badi <badialbahra@gmail.com>
2024-11-03 19:00:29 +0100

    added environment class

---
+++ src/pr2mapAgent/Environment.java
@@ -0,0 +1,17 @@
+package pr2mapAgent;
+
+public class Environment {
+
+    Map map;
+    int[][] path;
+    int[][] startPos;
+    int[][] targetPos;
+    int[][] currentPos;
+
+    public Environment(Map map) {
+        this.map = map;
+    }
+
+
+
+}
