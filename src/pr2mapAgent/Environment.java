c03e9e34d12759006c27ce5a72b3a5f079b5b899
Badi <badialbahra@gmail.com>
2024-11-03 19:22:20 +0100

    initialised set start and target method

--- src/pr2mapAgent/Environment.java
+++ src/pr2mapAgent/Environment.java
@@ -1,17 +1,28 @@
 package pr2mapAgent;
 
+import java.util.Random;
+
 public class Environment {
 
     Map map;
     int[][] path;
-    int[][] startPos;
-    int[][] targetPos;
-    int[][] currentPos;
+    int[] startPos;
+    int[] targetPos;
+    int[] currentPos;
 
     public Environment(Map map) {
         this.map = map;
+        startPos = new int[2];
+        targetPos = new int[2];
     }
 
+    void setStartAndTarget() {
+        Random rand = new Random();
+//        while ()
+        startPos[0] = rand.nextInt(map.getWidth());
+        startPos[1] = rand.nextInt(map.getHeight());
 
+
+    }
 
 }
