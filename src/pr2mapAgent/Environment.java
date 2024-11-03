a011191ad9c59f6e3ae4902d44da625ad7f893c0
Badi <badialbahra@gmail.com>
2024-11-03 20:14:06 +0100

    completed setStartAndTarget() and added printEnvironment()

--- src/pr2mapAgent/Environment.java
+++ src/pr2mapAgent/Environment.java
@@ -9,20 +9,54 @@ public class Environment {
     int[] startPos;
     int[] targetPos;
     int[] currentPos;
+    int height;
+    int width;
 
     public Environment(Map map) {
         this.map = map;
-        startPos = new int[2];
-        targetPos = new int[2];
+
+        this.height = map.getHeight();
+        this.width = map.getWidth();
+
+        startPos = new int[]{0, 0};
+        targetPos = new int[]{0, 0};
+        setStartAndTarget();
+        currentPos = startPos.clone();
     }
 
     void setStartAndTarget() {
         Random rand = new Random();
-//        while ()
-        startPos[0] = rand.nextInt(map.getWidth());
-        startPos[1] = rand.nextInt(map.getHeight());
+        do {
+            startPos[0] = rand.nextInt(height);
+            startPos[1] = rand.nextInt(width);
+        }
+        while (map.getMatrix()[startPos[0]][startPos[1]] == -1);
 
+        do {
+            targetPos[0] = rand.nextInt(height);
+            targetPos[1] = rand.nextInt(width);
+        }
+        while (map.getMatrix()[targetPos[0]][targetPos[1]] == -1);
+    }
 
+    void printEnvironment() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i == startPos[0] && j == startPos[1]) {
+                    System.out.print("[S]");
+                    continue;
+                }
+                if (i == targetPos[0] && j == targetPos[1]) {
+                    System.out.print("[T]");
+                    continue;
+                }
+                if (map.getMatrix()[i][j] == 0) {
+                    System.out.print(" ");
+                }
+                System.out.print(map.getMatrix()[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
 }
